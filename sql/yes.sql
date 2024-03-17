c5ff261e6b27aa97682a9dbe34450ebcd6070df0
mikrozhenek <zheka0723@gmail.com>
2024-03-17 22:57:48 +0300

    remove sql files

--- sql/yes.sql
+++
@@ -1,9 +0,0 @@
--- Вставка данных о фильмах с указанием массива идентификаторов актеров
-INSERT INTO movies (title, description, release_date, rating, actors_id)
-VALUES ('Фильм 1', 'Описание фильма 1', '2022-01-01', 8.5, ARRAY[1, 2]), -- Фильм 1 с участием актеров с идентификаторами 1 и 2
-       ('Фильм 3', 'Описание фильма 2', '2023-05-15', 0.0, ARRAY[2]);   -- Фильм 2 с участием только актера с идентификатором 2
-
-
-INSERT INTO users (email, password) VALUES ('zhiborkin@ya.ru', 'kekus123'), ('kekus@mail.ru', 'lolip12344')
-
-DROP TABLE users;
--- sql/yes2.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO actors (name, sex, birthday, movies_id)
-VALUES ('Евгений', 'мужской', '2002-01-01', ARRAY[1]),
-       ('Олег', 'женский', '2003-05-15', ARRAY[1, 2]);
